d5c1a054b86db9fd74afd1dc74c7fad096167608
Kerstin Zauke <kzauke@accuraty.com>
2018-03-13 07:53:22 -0500

    initial commit

---
+++ src/util/Yelp.js
@@ -0,0 +1,47 @@
+const clientId = 'DGJl9REkayBOaAWwrNZTpw';
+const secret = 'SS1v3cEZ1wN6r7O6ceKhQaxN07Kbj9hsf0jYNwmeZkCtkDK5D4SO2hgvtf1FjVz7';
+
+let accessToken = '';
+
+const Yelp = {
+	getAccessToken() {
+		if (accessToken) {
+			return new Promise(resolve => resolve(accessToken));
+		}
+		return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_id=${clientId}&client_secret=${secret}`, {
+			method: 'POST'
+		})
+		.then(response => response.json())
+		.then(jsonResponse => accessToken = jsonResponse.access_token);
+	},
+
+	search(term, location, sortBy) {
+		const url = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`;
+		return Yelp.getAccessToken().then(() => {
+			return fetch(url, {
+				headers: {Authorization: `Bearer ${accessToken}`},
+			});
+		})
+		.then(response => response.json())
+		.then(jsonResponse => {
+			if (jsonResponse.businesses) {
+				return jsonResponse.businesses.map(business => {
+					return {
+						id: business.id,
+						imageSrc: business.image_url,
+						name: business.name,
+						address: business.location.address1,
+						city: business.location.city,
+						state: business.location.state,
+						zipCode: business.location.zip_code,
+						category: business.categories.title,
+						rating: business.rating,
+						reviewCount: business.review_count
+					}
+				})
+			}
+		})
+	},
+}
+
+export default Yelp;
